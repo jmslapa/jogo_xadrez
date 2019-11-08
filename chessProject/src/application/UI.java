6b5da490afa41d7e30dc70159daa2167a88d971b
jmslapa <lapeta96@gmail.com>
2019-11-08 13:49:35 -0300

    <ALTERAÇÕES>
    application.Program:
    	Adicionado método que limpa tela antes de imprimir o tabuleiro dentro do laço 'while'
    	Adicionado tratamento para ChessException e InputMismatchException
    
    application.UI:
    	criado método 'clearScreen'

--- chessProject/src/application/UI.java
+++ chessProject/src/application/UI.java
@@ -8,7 +8,7 @@ import chess.ChessPosition;
 import chess.Color;
 
 public class UI {
-	
+
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_BLACK = "\u001B[30m";
 	public static final String ANSI_RED = "\u001B[31m";
@@ -27,41 +27,45 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
-	
+
 	public static ChessPosition readChessPosition(Scanner in) {
 		try {
 			String input = in.nextLine();
 			char column = input.charAt(0);
 			int row = Integer.parseInt(input.substring(1));
 			return new ChessPosition(column, row);
-		}catch(RuntimeException e) {
+		} catch (RuntimeException e) {
 			throw new InputMismatchException("Error reading chess position: valid values are from a1 to h8");
-		}		
+		}
 	}
-	
+
 	public static void printBoard(ChessPiece[][] pieces) {
-		for(int i = 0 ; i < pieces.length ; i++) {
-			System.out.print((8-i) + " ");
-			for(int j = 0 ; j < pieces[i].length ; j++) {
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces[i].length; j++) {
 				printPiece(pieces[i][j]);
 			}
 			System.out.println();
 		}
 		System.out.println("  a b c d e f g h");
 	}
-	
-	public static void printPiece (ChessPiece piece) {
-		if(piece == null) {
+
+	public static void printPiece(ChessPiece piece) {
+		if (piece == null) {
 			System.out.print("-");
-		}else {
+		} else {
 			if (piece.getColor() == Color.WHITE) {
-                System.out.print(ANSI_RED + piece + ANSI_RESET);
-            }
-            else {
-                System.out.print(ANSI_BLACK + piece + ANSI_RESET);
-            }
+				System.out.print(ANSI_RED + piece + ANSI_RESET);
+			} else {
+				System.out.print(ANSI_BLACK + piece + ANSI_RESET);
+			}
 		}
-		
+
 		System.out.print(" ");
+	}
+
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
 	}
 }
