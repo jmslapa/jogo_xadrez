80e851f38493fe50e034bb5daa227d04d15b710d
jmslapa <lapeta96@gmail.com>
2019-11-10 16:13:16 -0300

    <LÓGICA DE XEQUE-MATE>
    application.ChessMatch:
    	Adicionado atributo checkMate
    	Criado método getCheckMate
    	Criado método testCheckMate
    	Alterado método performChessMove
    
    application.UI:
    	Alterado método printMatch
    
    application.Program:
    	Alterada condicional do laço while
    	Alterada lógica da classe, de forma a permitir o encerramento do jogo

--- chessProject/src/application/UI.java
+++ chessProject/src/application/UI.java
@@ -48,9 +48,14 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turns: " + chessMatch.getTurns());
-		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-		if(chessMatch.getCheck()) {
-			System.out.println("CHECK!");
+		if(!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+			if(chessMatch.getCheck()) {
+				System.out.println("CHECK!");
+			}
+		}else {
+			System.out.println("CHECKMATE!");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 		}
 	}
 
